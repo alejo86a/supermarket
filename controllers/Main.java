cd2e8172a5466cc64d9ae3c79b8740cfe3d64a89
Alejandro Berrio <alejandro.berrio@rappi.com>
2019-09-01 17:40:15 -0500

    logic to calculate points on supermarket

--- controllers/Main.java
+++ controllers/Main.java
@@ -1,33 +1,24 @@
 package controllers;
 
-import data.*;
-import java.io.BufferedReader;
+import controllers.SuperMarket;
+import data.ReadFile;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
 
-    public static void muestraContenido(String archivo) throws IOException {
-        try {
-            String cadena;
-            FileReader f = new FileReader(archivo);
-            BufferedReader b = new BufferedReader(f);
-            while((cadena = b.readLine())!=null) {
-                System.out.println(cadena);
-            }
-            b.close();
-        }catch(IOException fe) {
-            System.out.println(fe);    
-        }
-       
-    }
+    public static SuperMarket superMarket;
+    public static ReadFile readFile;
 
+    public Main()  throws FileNotFoundException, IOException  {
+        superMarket = new SuperMarket();
+        ReadFile readFile = new ReadFile(this);        
+        readFile.muestraContenido("C:/Users/Usuario/Documents/ITM/Laboratorio estructuras/supermarket/sales.txt");
+    }
 
     public static void main(String args[])  throws FileNotFoundException, IOException {
         System.out.println("hello world");
-        ReadFile readFile = new ReadFile();
-        readFile.muestraContenido("C:/Users/Usuario/Documents/ITM/Laboratorio estructuras/supermarket/sales.txt");
+        Main main = new Main();
 
     }
 }
